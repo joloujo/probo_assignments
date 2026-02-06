7a8d301a35155b6f24b16b2502934a0aff4d1582
Dominic Salmieri <dsalmieri@olin.edu>
2026-02-06 11:36:56 -0500

    Day 5 exercise 1: changed variable names to match equation

--- src/day5/exercise1.py
+++ src/day5/exercise1.py
@@ -7,10 +7,12 @@ prior = np.array([0.985, 0.015])
 # Because our observation is always the same in this case, we don't have to use the whole matrix
 likelihood = np.array([1/4, 1])
 
+print(f'Timestep {0}: {prior = }')
+
 for i in range(10):
-    state = likelihood * prior
-    state = state / np.sum(state)
+    posterior = likelihood * prior
+    posterior = posterior / np.sum(posterior)
 
-    print(f'Timestep {i}: {state = }')
+    print(f'Timestep {i + 1}: {posterior = }')
 
-    prior = state
+    prior = posterior
